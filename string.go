35750cfcce2999047d77e8e689e5c1b56d87796f
anjar-wilujeng <anjarwilujeng8@gmail.com>
2025-05-30 09:12:52 +0700

    Initial commit

---
+++ string.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Tipe data string")
+	fmt.Println("Belajar go lang dasar")
+	fmt.Println(len("golang"))
+	fmt.Println("golang"[0])
+	fmt.Println("golang"[1])
+}
