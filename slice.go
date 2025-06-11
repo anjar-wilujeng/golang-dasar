9f7b23605f9c1d9aa81f2229b75b5ec6b54b1b1e
awesome <anjarwilujeng8@gmail.com>
2025-06-11 21:50:50 +0700

    update slice

--- slice.go
+++ slice.go
@@ -33,4 +33,37 @@ func main() {
 	fmt.Println(daySlice2)
 	fmt.Println(daySlice1)
 	fmt.Println(days)
+
+	var newSlice []string = make([]string, 2, 5)
+
+	newSlice[0] = "Budi"
+	newSlice[1] = "Budi"
+
+	fmt.Println(newSlice)
+	fmt.Println(len(newSlice))
+	fmt.Println(cap(newSlice))
+
+	newSlice2 := append(newSlice, "Budi")
+
+	fmt.Println(newSlice2)
+	fmt.Println(len(newSlice2))
+	fmt.Println(cap(newSlice2))
+
+	newSlice2[0] = "Joko"
+	fmt.Println(newSlice2)
+	fmt.Println(newSlice)
+
+	fromSlice := days[:]
+	toSlice := make([]string, len(fromSlice), cap(fromSlice))
+
+	copy(toSlice, fromSlice)
+
+	fmt.Println(fromSlice)
+	fmt.Println(toSlice)
+
+	iniArray := [...]int{1, 2, 3, 4, 5}
+	iniSlice := []int{1, 2, 3, 4, 5}
+
+	fmt.Println(iniArray)
+	fmt.Println(iniSlice)
 }
